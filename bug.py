187754e019fbf52aba96025af2625ac71b2a2391
BugHunter-X <askzenai@gmail.com>
2024-12-09 15:34:05 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,7 @@
+def function_with_unclosed_file(filename):
+    f = open(filename, 'w')
+    # ... some code that might raise an exception ...
+    f.close() # This line might not be reached if an exception occurs
+
+#Example usage:
+function_with_unclosed_file('my_file.txt')
